ec23913b0added2ba1ec30065dfaf80db1d70bc8
Emanuele DelBono <emanuele@codiceplastico.com>
2013-10-14 11:20:35 +0200

    added a comment

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var sql = require('msnodesql');
+// dont't mind the password. Is on my local laptop, so who cares! ;-)
 var connectionString = "Driver={SQL Server Native Client 11.0};Server=127.0.0.1;Database=wkdo;Uid=sa;Pwd=CodicePlastico";;
 var testQuery = "SELECT * FROM Cliente";
 
